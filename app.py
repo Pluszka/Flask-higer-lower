bc6983cf570169501e592aac8c0502f6e0f756d0
Pliszka <karoliniakkjk@gmail.com>
2022-04-02 00:43:33 +0200

    gifs

--- app.py
+++ app.py
@@ -2,14 +2,28 @@ from flask import Flask
 from random import randint
 app = Flask(__name__)
 
-ANSWER = randint(0, 9)
+answer = randint(0, 9)
+BAD_ANSWER = "https://media.giphy.com/media/NpL4D3Oc2bJUMAXF9P/giphy.gif"
+GOOD_ANSWER = "https://media.giphy.com/media/o75ajIFH0QnQC3nCeD/giphy.gif"
 
 @app.route('/<int:number>')
 def number_site(number):
-    if ANSWER == number:
-        return 'Grats'
+    global answer
+    if answer == number:
+        return '<center>' \
+               f'<h2>U guess, the answer is {number}</h2>' \
+               f'<img src={GOOD_ANSWER}>' \
+               '</center>'
+    elif answer > number:
+        return '<center>' \
+               '<h2>Number is higher.</h2>' \
+               f'<image src={BAD_ANSWER}>' \
+               '</center>'
     else:
-        return 'Try'
+        return '<center>' \
+               '<h2>Number is lower.</h2>' \
+               f'<image src={BAD_ANSWER}>' \
+               '</center>'
 
 @app.route('/')
 def start():
