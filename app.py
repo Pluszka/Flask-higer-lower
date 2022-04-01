d667479708ffa8cd978df623038713baf94fe0fd
Pliszka <karoliniakkjk@gmail.com>
2022-04-01 23:16:41 +0200

    home page

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello'
+def start():
+    return '<center>' \
+           '<h1>Guess a number between 0 and 9</h1>' \
+           '<img src="https://media.giphy.com/media/VM9S5IeIK5hFm/giphy.gif">' \
+           '</center>'
 
 if __name__ == '__main__':
     app.run(debug=True)
